1444a71ca43eae09c84a99d10fee281c345c8388
Juan Pablo Calao Perez <juan.calao@ideaware.co>
2019-08-27 14:29:52 -0500

    search input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const handleSubmit = query => {
+  console.log(query)
+}
+
 function App() {
+  const [query, setQuery] = useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={e => {
+        e.preventDefault()
+        handleSubmit(query)
+      }}>
+        <input
+          type="text"
+          placeholder="'react'"
+          value={query}
+          onChange={e => {setQuery(e.target.value)}} />
+        <input type="submit" value="Search" />
+      </form>
     </div>
   );
 }
