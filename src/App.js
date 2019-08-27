01cbb76a9d96af77371230523751e05a8ec48d2c
Juan Pablo Calao Perez <juan.calao@ideaware.co>
2019-08-27 14:51:27 -0500

    with useEffect and infinite fetch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [query, setQuery] = useState('')
   const [articles, setArticles] = useState([])
 
-  const handleInputChange = inputValue => {
-    setQuery(inputValue)
+  const fetchArticles = () => {
     const fetchUrl = `https://hn.algolia.com/api/v1/search?query=${query}`
     fetch(fetchUrl)
       .then(res => res.json())
       .then(data => setArticles(data.hits))
       .catch(err => console.log(err))
   }
+
+  useEffect(fetchArticles)
 
   return (
     <div className="App">
@@ -23,7 +24,7 @@ function App() {
           type="text"
           placeholder="'react'"
           value={query}
-          onChange={e => {handleInputChange(e.target.value)}} />
+          onChange={e => {setQuery(e.target.value)}} />
         <input type="submit" value="Search" />
       </form>
       <ul>
